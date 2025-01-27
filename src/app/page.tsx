a7f6d9ef98ba0380fb91467601b0879e94e5d6bf
Suresh <sureshrao100000@gmail.com>
2025-01-27 19:42:05 +0530

    Update Course Interface page.tsx component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ export default function Home() {
     image: string;
     name: string;
     isActive: boolean;
+    releaseDate: string;
   }
 
   const [courses, setCourses] = useState<Course[]>([]);
